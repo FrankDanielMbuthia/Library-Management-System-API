5a88697203c6f396756aef9429e1df6b10c422fa
Frank Daniel <mbuthiawanganju@gmail.com>
2026-01-12 09:34:15 +0300

    Added checkout custom action to book viewset

--- library/services.py
+++ library/services.py
@@ -1,2 +1,15 @@
+from django.core.exceptions import ValidationError
+from .models import Transaction
+
 def checkout_book(user, book):
-    pass
+    if not book.is_available():
+        raise ValidationError("This book is not available for checkout.")
+    
+    if Transaction.objects.filter(user=user,book=book, returned_at_isnull=True).exists():
+        raise ValidationError("User has already checked out this book and not returned it yet.")
+    
+    transaction = Transaction.objects.create(user=user, book=book)
+    book.number_of_copies -= 1
+    book.save()
+    return transaction
+    
--- library/views.py
+++ library/views.py
@@ -1,11 +1,33 @@
 from rest_framework import viewsets
 from .models import Book, Transaction
 from .serializers import BookSerializer, TransactionSerializer
+from rest_framework.decorators import action #Creates a custom endpoint
+from rest_framework.response import Response
+from rest_framework import status #HTTP status codes
+from django.core.exceptions import ValidationError
+from .services import checkout_book
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    @action(detail=True, methods=['post'])
+    def checkout(self, request, pk=None):
+        book = self.get_object()
+
+        try:
+            transaction = checkout_book(request.user, book)
+        except ValidationError as e:
+            return Response(
+                {"error":e.message},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return Response(
+            {"message":"Book checked out successfully"},
+            status= status.HTTP_200_OK
+        )
+
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    
